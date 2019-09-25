f78716ea4d1c787e880ed4bafb754f47b27fd3c2
Alberto Basalo <albertobasalo@hotmail.com>
2019-09-25 19:28:04 +0200

    onpush observable

--- src/app/core/countries.service.ts
+++ src/app/core/countries.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { Country } from '../shared/models/country.interface';
 
 @Injectable({
@@ -14,8 +14,7 @@ export class CountriesService {
     const url = 'http://api.worldbank.org/V2/country?per_page=1000&format=json';
     return this.http.get<any>(url).pipe(
       map(res => res[1]),
-      map(apiArray => apiArray.map(item => ({ ...item, region: item.region.value }))),
-      tap(x => console.log(x))
+      map(apiArray => apiArray.map(item => ({ ...item, region: item.region.value })))
     );
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { CountriesService } from '../core/countries.service';
 import { CountryCounterStoreService } from '../core/country-counter-store.service';
 import { Country } from '../shared/models/country.interface';
@@ -10,21 +12,23 @@ import { Country } from '../shared/models/country.interface';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent implements OnInit {
-  public countries: Country[];
-  private allCountries: Country[] = [];
+  public countries$: Observable<Country[]>;
+  public allCountries: Country[];
 
   constructor(private store: CountryCounterStoreService, private countriesServices: CountriesService) {}
 
   public ngOnInit() {
-    this.countriesServices.getAll$().subscribe(res => {
-      this.allCountries = res;
-      this.countries = [...this.allCountries];
-      this.store.set({ numCountries: this.countries.length });
-    });
+    this.countries$ = this.countriesServices.getAll$().pipe(
+      tap(items => {
+        this.allCountries = items;
+        this.store.set({ numCountries: items.length });
+      })
+    );
   }
 
   public filterCountries(countryName: string) {
-    this.countries = this.allCountries.filter(c => c.name.includes(countryName));
-    this.store.set({ numCountries: this.countries.length });
+    const filtrados = this.allCountries.filter(c => c.name.includes(countryName));
+    this.store.set({ numCountries: filtrados.length });
+    this.countries$ = of(filtrados);
   }
 }
