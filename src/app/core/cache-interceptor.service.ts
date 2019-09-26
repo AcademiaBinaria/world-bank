876455cd2456c7f748ac1013093f6d7b28e5069d
Alberto Basalo <albertobasalo@hotmail.com>
2019-09-26 18:48:54 +0200

    cache

---
+++ src/app/core/cache-interceptor.service.ts
@@ -0,0 +1,35 @@
+import { HttpEvent, HttpHandler, HttpRequest, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CacheInterceptorService {
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const cachedResponse = this.cacheGet(request.url);
+    if (cachedResponse) {
+      return of(new HttpResponse({ body: cachedResponse }));
+    } else {
+      return next.handle(request).pipe(
+        tap(event => {
+          if (event instanceof HttpResponse) {
+            this.cacheSet(request.url, event);
+          }
+        })
+      );
+    }
+  }
+  cacheSet(url: string, event: HttpEvent<any>) {
+    if (event instanceof HttpResponse) {
+      localStorage.setItem(url, JSON.stringify(event.body));
+    }
+  }
+  cacheGet(url: string) {
+    const evt = localStorage.getItem(url);
+    return evt ? JSON.parse(evt) : null;
+  }
+
+  constructor() {}
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,7 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ApiInterceptorService } from './api-interceptor.service';
+import { CacheInterceptorService } from './cache-interceptor.service';
 import { ErrorInterceptorService } from './error-interceptor.service';
 import { CountryCounterComponent } from './shell/header/country-counter/country-counter.component';
 import { HeaderComponent } from './shell/header/header.component';
@@ -12,6 +13,7 @@ import { ShellComponent } from './shell/shell.component';
   declarations: [ShellComponent, HeaderComponent, CountryCounterComponent],
   imports: [CommonModule, RouterModule, HttpClientModule],
   providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: CacheInterceptorService, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: ApiInterceptorService, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptorService, multi: true }
   ],
